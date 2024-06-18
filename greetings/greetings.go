eebe5f77157fe0ae935c031f6e2e8dabdd903bec
abosaeed <eng.abdelrahman.M.saeed@gmail.com>
2024-06-18 03:20:10 +0300

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	// Return a greeting that embeds the name in message.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hello, %v. Welcome!", name)
-	return message
+	return message, nil
 }
