0b79782d31afa76f436df64ad704e015386d060d
abdelrahman mostafa mohamed saeed <97610820+abdelrahmanMsaeed@users.noreply.github.com>
2024-06-23 07:14:48 +0300

    add random greeting messages.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -10,8 +11,21 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
+
 	// If a name was received, return a value that embeds the name
 	// in a greeting message.
-	message := fmt.Sprintf("Hello, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Random Format returns one of a set of greetings messages.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great To See You, %v!",
+		"Hi, %v. Welcome!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -10,8 +10,8 @@ func main() {
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
+	log.SetPrefix("greetings Module: ")
+	//log.SetFlags(0)
 	// Request a greeting message.
 	message, err := greetings.Hello("Saeed")
 	// If an error was returned, print it to the console and
